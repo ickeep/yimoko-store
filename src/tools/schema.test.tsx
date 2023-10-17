e4c5851ddbeebaf39151d99581a1fafe8f0fe2cf
keepcai <keepcai@tencent.com>
2023-10-17 15:12:12 +0800

    getItemPropsBySchema 单元测试

--- src/tools/schema.test.tsx
+++ src/tools/schema.test.tsx
@@ -1,108 +1,113 @@
-import { createSchemaField, ISchema } from '@formily/react';
+import { createSchemaField, ISchema, useFieldSchema } from '@formily/react';
 import { render, screen } from '@testing-library/react';
 
 import { SchemaPage } from '../components/schema-page';
 import { SchemaFieldProvider } from '../context/schema-field';
 
 import { getItemPropsBySchema } from './schema';
+import { judgeIsEmpty } from './tool';
 
 
-describe('useSchemaItemsOut', () => {
-  test('useSchemaItemsOut', () => { });
-  // const Render = ({ schema }: { schema?: ISchema }) => {
-  //   const components = {
-  //     A: ({ componentName, schemaKey }: { componentName: string, schemaKey: string | number }) => {
-  //       const items = useSchemaItems();
-  //       return <div> {items.map((item, i) => <div key={i} {...getItemPropsBySchema(item, componentName, schemaKey)} />)}</div>;
-  //     },
-  //   };
-  //   const SchemaField = createSchemaField({ components });
-  //   return (
-  //     <SchemaFieldProvider value={SchemaField}>
-  //       <SchemaPage components={components} schema={schema} />
-  //     </SchemaFieldProvider>
-  //   );
-  // };
-
-  // test('componentName === component', () => {
-  //   render(<Render schema={{
-  //     type: 'object', properties: {
-  //       A: {
-  //         'x-component': 'A',
-  //         'x-component-props': { componentName: 'div' },
-  //         items: { 'x-component': 'div', 'x-component-props': { children: 'div' } },
-  //       },
-  //     },
-  //   }} />);
-  //   expect(screen.getByText('div')).toBeInTheDocument();
-  // });
-
-  // test('componentName ===component   has properties', () => {
-  //   render(<Render schema={{
-  //     type: 'object', properties: {
-  //       A: {
-  //         'x-component': 'A',
-  //         'x-component-props': { componentName: 'div' },
-  //         items: {
-  //           'x-component': 'div',
-  //           properties: {
-  //             div: { 'x-component': 'div', 'x-component-props': { children: 'div' } },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   }} />);
-  //   expect(screen.getByText('div')).toBeInTheDocument();
-  // });
+describe('getItemPropsBySchema', () => {
+  const Render = ({ schema }: { schema?: ISchema }) => {
+    const components = {
+      A: ({ componentName, data }: { componentName: string, schemaKey: string | number, data?: any[] }) => {
+        const schema = useFieldSchema();
+        const { items } = schema;
+        const schemaItem = Array.isArray(items) ? (items[0]) : items;
+        if (judgeIsEmpty(schemaItem)) {
+          return null;
+        }
+        return <div>
+          {data?.map((item, index) => {
+            const { children, ...rest } = getItemPropsBySchema(schemaItem, componentName, index);
+            return (
+              <div key={index}>
+                <div role='children'>{children}</div>
+                <div role='rest'>{JSON.stringify(rest)}</div>
+              </div>
+            );
+          })}
+        </div>;
+      },
+    };
+    const SchemaField = createSchemaField({ components });
+    return (
+      <SchemaFieldProvider value={SchemaField}>
+        <SchemaPage components={components} schema={schema} />
+      </SchemaFieldProvider>
+    );
+  };
 
 
-  // test('decorator props', () => {
-  //   render(<Render schema={{
-  //     type: 'object', properties: {
-  //       A: {
-  //         'x-component': 'A',
-  //         'x-component-props': { componentName: 'div' },
-  //         items: { 'x-decorator-props': { 'data-testid': 'div' } },
-  //       },
-  //     },
-  //   }} />);
-  //   expect(screen.getByTestId('div')).toBeInTheDocument();
-  // });
+  test('empty', () => {
+    render(<Render schema={{
+      type: 'object',
+      properties: {
+        A: {
+          'x-component': 'A',
+          'x-component-props': { componentName: 'div', data: ['1'] },
+        },
+      },
+    }} />);
+    expect(document.body.textContent).toBe('');
+  });
 
-  // test('decorator props decorator === componentName', () => {
-  //   render(<Render schema={{
-  //     type: 'object', properties: {
-  //       A: {
-  //         'x-component': 'A',
-  //         'x-component-props': { componentName: 'div' },
-  //         items: { 'x-decorator': 'div', 'x-decorator-props': { 'data-testid': 'div1' } },
-  //       },
-  //     },
-  //   }} />);
-  //   expect(screen.getByTestId('div1')).toBeInTheDocument();
-  // });
+  test('children', () => {
+    render(<Render schema={{
+      type: 'object',
+      properties: {
+        A: {
+          'x-component': 'A',
+          'x-component-props': { componentName: 'div', data: ['1'] },
+          items: {
+            type: 'string',
+            'x-decorator-props': { k1: '1', k2: '2' },
+            'x-component': 'span',
+            'x-component-props': { children: 'span' },
+          },
+        },
+      },
+    }} />);
+    expect(screen.getByRole('children')).toHaveTextContent('span');
+    expect(screen.getByRole('rest')).toHaveTextContent('{"k1":"1","k2":"2"}');
+  });
 
-  // test('decorator !== componentName', async () => {
-  //   render(<Render schema={{
-  //     type: 'object', properties: {
-  //       A: {
-  //         'x-component': 'A',
-  //         'x-component-props': { componentName: 'div' },
-  //         items: {
-  //           // 'x-decorator' 无效的
-  //           'x-decorator': 'button',
-  //           'x-decorator-props': { className: 'button' },
-  //           'x-component': 'button',
-  //           properties: {
-  //             button: { type: 'void', 'x-component': 'button', 'x-component-props': { children: 'button' } },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   }} />);
-  //   expect(screen.getByText('button')).toBeInTheDocument();
+  test('decorator no some', () => {
+    render(<Render schema={{
+      type: 'object',
+      properties: {
+        A: {
+          'x-component': 'A',
+          'x-component-props': { componentName: 'div', data: ['1'] },
+          items: {
+            type: 'string',
+            'x-decorator': 'span',
+            'x-component': 'span',
+            'x-component-props': { children: 'span' },
+          },
+        },
+      },
+    }} />);
+    expect(screen.getByRole('children')).toHaveTextContent('span');
+    expect(screen.getByRole('rest')).toHaveTextContent('{}');
+  });
 
-  //   // eslint-disable-next-line testing-library/no-node-access
-  //   expect(document.querySelector('.button')).toBeNull();
-  // });
+  test('not component', () => {
+    render(<Render schema={{
+      type: 'object',
+      properties: {
+        A: {
+          'x-component': 'A',
+          'x-component-props': { componentName: 'div', data: ['1'] },
+          items: {
+            type: 'string',
+            'x-decorator': 'span',
+            'x-decorator-props': { k1: '1', k2: '2', children: 'span' },
+          },
+        },
+      },
+    }} />);
+    expect(document.body.textContent).toBe('{}');
+  });
 });
--- src/tools/schema.tsx
+++ src/tools/schema.tsx
@@ -4,11 +4,12 @@ import { Key } from 'react';
 import { judgeIsEmpty } from './tool';
 
 // 用于在有子项项目，例如 menu 之类，获取 schema item 子项配置
+// 约定使用 decorator 作为组件名 且 decorator 为空或者等于当前组件名时， decoratorProps 做为组件的 props
 export function getItemPropsBySchema(schema: Schema, componentName: string, dataIndex?: number): Record<Key, any> {
   const {
     'x-component': component,
     'x-decorator': decorator,
-    'x-decorator-props': decoratorProps,
+    'x-decorator-props': decoratorProps = {},
     name,
     ...args
   } = schema;
